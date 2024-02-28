32cf7c82c2a3f740b48c0471eb7a4d1542aea7f6
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 <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>
2024-02-28 01:49:47 -0800

    updated BaseModel to re-create an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,21 @@ class BaseModel:
     """Base class"""
 
     def __init__(self, *args, **kwargs):
-        """Constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """Constructor method to re-create an instance"""
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            if len(kwargs) != 0:
+                for k, v in kwargs.items():
+                    if k == "__class__":
+                        continue
+                    if k == "created_at" or k == "updated_at":
+                        self.__dict__[k] = datetime.strptime(v, timeformat)
+                    else:
+                        self.__dict__[k] = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the class"""
