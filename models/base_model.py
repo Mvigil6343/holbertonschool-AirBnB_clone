7eb6a8745a195f2e2f25bbab31cdab174e26be9d
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 <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>
2024-02-28 00:56:41 -0800

    a class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Class defines all common attributes/methods for other classes"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class"""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor method"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the class"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updat <updated_at> with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values"""
+        dicti = self.__dict__.copy()
+        dicti["__class__"] = type(self).__name__
+        dicti["created_at"] = self.created_at.isoformat()
+        dicti["updated_at"] = self.updated_at.isoformat()
+        return dicti
