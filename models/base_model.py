2b178770f43916f6314373e6f3bd2ef1d40b6b6d
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 <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>
2024-02-28 18:30:42 -0800

    update storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Class defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -23,14 +24,16 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """String representation of the class"""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """updat <updated_at> with the current datetime"""
+        """update <updated_at> with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
