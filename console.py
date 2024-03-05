1dbe0a59745dbd7dc36494d1b86ea16f5bb5b05b
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 <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>
2024-03-05 09:35:57 -0800

    removed unnecesary imports

--- console.py
+++ console.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-import sys
 from models.base_model import BaseModel
-from datetime import datetime
 from models.__init__ import storage
 from models.engine.file_storage import FileStorage
 from models.state import State
